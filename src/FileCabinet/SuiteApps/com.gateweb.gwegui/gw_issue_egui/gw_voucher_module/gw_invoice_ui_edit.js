f4ee39ca0954f000e7fb4ee3c6c24e2a71128f89
Chesley Lo <Chesleylo@gateweb.com.tw>
2023-12-20 16:27:17 +0800

    Adding switch for EGUI department, class
    (cherry picked from commit 984b8275c689f4418cc790aabc78e7fa3aaeece5)

--- src/FileCabinet/SuiteApps/com.gateweb.gwegui/gw_issue_egui/gw_voucher_module/gw_invoice_ui_edit.js
+++ src/FileCabinet/SuiteApps/com.gateweb.gwegui/gw_issue_egui/gw_voucher_module/gw_invoice_ui_edit.js
@@ -1465,12 +1465,12 @@ define([
     var _dept_codeField = form.getField({
       id: 'custpage_dept_code'
     })
-    // _dept_codeField.defaultValue = _default_department_id
+    if(isEGUIDepartmentEnable) _dept_codeField.defaultValue = _default_department_id
 
     var _classificationField = form.getField({
       id: 'custpage_classification'
     })
-    // _classificationField.defaultValue = _selectClassification
+    if(isEGUIClassEnable) _classificationField.defaultValue = _selectClassification
 
     var _voucherExtraMemoField = form.getField({
       id: 'custpage_voucher_extra_memo'
@@ -2438,12 +2438,12 @@ define([
     var _dept_codeField = form.getField({
       id: 'custpage_dept_code'
     })
-    // _dept_codeField.defaultValue = _default_department_id
+    if(isEGUIDepartmentEnable) _dept_codeField.defaultValue = _default_department_id
 
     var _classificationField = form.getField({
       id: 'custpage_classification'
     })
-    // _classificationField.defaultValue = _selectClassification
+    if(isEGUIClassEnable) _classificationField.defaultValue = _selectClassification
     log.debug(
       '_customer_id',
       '_customer_id=' +
@@ -2606,6 +2606,8 @@ define([
       title: 'onRequest - eGUIConfig',
       details: eGUIConfig
     })
+    isEGUIClassEnable = eGUIConfig.isEGUIClass
+    isEGUIDepartmentEnable = eGUIConfig.isEGUIDepartment
 
     if (_selected_invoice_Id != null) {
       var _idAry = _selected_invoice_Id.split(',')
