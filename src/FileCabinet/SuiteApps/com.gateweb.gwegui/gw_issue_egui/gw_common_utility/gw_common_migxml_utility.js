e45574b14f474a47e5c5e6753584d26a2abcc84d
Chesley Lo <Chesleylo@gateweb.com.tw>
2025-06-12 14:37:56 +0800

    fix: 當第一個商品備註為空時第二個商品備註沒顯示之問題, 以及沒有按照sequence顯示之問題
    (cherry picked from commit 6e2a8aed5f16d977fab1aa535f2ddae057da4aca)

--- src/FileCabinet/SuiteApps/com.gateweb.gwegui/gw_issue_egui/gw_common_utility/gw_common_migxml_utility.js
+++ src/FileCabinet/SuiteApps/com.gateweb.gwegui/gw_issue_egui/gw_common_utility/gw_common_migxml_utility.js
@@ -67,6 +67,9 @@ define([
         xpath: 'Invoice',
       })
 
+      if(description.length === 0) description = ' '
+      if(itemremark.length === 0) itemremark = ' '
+
       for (var i = 0; i < _migInvoiceNode.length; i++) {
         var _childNodes = _migInvoiceNode[i].childNodes
 
@@ -1203,6 +1206,11 @@ define([
       ])
 
       _mySearch.filterExpression = _filterArray
+      _mySearch.columns.push(search.createColumn({
+        name: 'custrecord_gw_item_seq',
+        join: 'custrecord_gw_voucher_main_internal_id',
+        sort: search.Sort.ASC
+      }))
 
       var _indexId = ''
       var _isFirst = true
